603c165bd569ab17cec807650eeb393a373c959c
guths <jonathanguths@gmail.com>
2021-04-22 22:55:39 -0300

    aprendendo umas paradinhas

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <base-spinner></base-spinner>
+    <button @click="hideSpinner()" class="btn btn-primary">Show Spinner</button>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import BaseSpinner from './components/BaseSpinner';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  components: {
+    BaseSpinner
+  },
+  methods: {
+    hideSpinner () {
+      this.$root.$emit('Spinner::show')
     }
   }
 }
+</script>
+
+<style lang="scss">
+
 </style>
---
+++ src/components/BaseSpinner.vue
@@ -0,0 +1,45 @@
+<template>
+  <div v-if="visible" class="base-spinner fa-3x">
+    <i class="fas fa-circle-notch fa-spin"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visible: false
+    }
+  },
+  created() {
+    this.$root.$on('Spinner::show', this.changeSpinner)
+
+    this.$root.$on('Spinner::hide', this.changeSpinner)
+  },
+  name: 'base-spinner',
+  methods: {
+    changeSpinner () {
+      this.visible = !this.visible;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/variables';
+
+.base-spinner {
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: $dark-medium;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  color: $featured;
+}
+</style>
